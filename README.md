50d5f2d3aae4af253577ae11430f86876108abf5
morestar <morestarforyou@gmail.com>
2018-08-11 23:46:07 +0800

    修复大量bug

--- README.md
+++ README.md
@@ -5,17 +5,20 @@
 
 或使用`sudo nano install.py`然后将内容复制进去
 ## 参数说明
-- `-wifi`添加wifi配置
-- `-version` 查看版本号
-- `-source` 更换pip apt源
-- `-prepare` 更新软件源与软件
-- `-installHA` 安装HomeAssistant
-- `-autostart` 添加自启动
-- `-samba` 安装并配置samba
-- `-chinese` 中文配置
-- `-mosquitto` MQTT Broker安装
+- `-w`添加wifi配置
+- `-v` 查看版本号
+- `-s` 更换pip apt源
+- `-p` 更新软件源与软件
+- `-i` 安装HomeAssistant
+- `-a` 添加自启动
+- `-sa` 安装并配置samba
+- `-c` 中文配置
+- `-m` MQTT Broker安装
+- `-u` 更新HomeAssistant版本及其依赖
 
 
 ## 示例
 
-`sudo python3 install.py -wifi -version`
+`sudo python3 install.py -w true -s true`
+
+`sudo python3 install.py -w True -s True`
--- install.py
+++ install.py
@@ -1,4 +1,3 @@
-import fileinput
 import subprocess
 
 
@@ -68,27 +67,27 @@ class Service:
         try:
             subprocess.run("sudo mv /etc/pip.conf /etc/pip.conf.bak")
         except FileNotFoundError:
-            Logger.error("can't find pip conf files")
+            Logger.error("can't find pip conf file, we will set up it.")
         finally:
             with open(self.pip_source, 'w+') as f:
                 f.write("[global]\n"
                         "index-url = https://pypi.tuna.tsinghua.edu.cn/simple")
-                Logger.info("write pip conf success")
+                Logger.info("write pip conf file success")
         try:
             subprocess.run("sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak")
         except FileNotFoundError:
-            Logger.error("can't find apt conf files")
+            Logger.error("can't find apt conf file, we will set up it.")
         finally:
             with open(self.apt_source, 'w+') as f:
                 f.write("deb http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ stretch main non-free contrib\n"
                         "deb-src http://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/ stretch main non-free contrib")
-                Logger.info("write apt source conf success")
+                Logger.info("write apt source conf file success")
 
     # 更新源与软件
     @staticmethod
     def prepare():
-        subprocess.run("sudo apt-get update")
-        subprocess.run("sudo apt-get upgrade")
+        subprocess.run("sudo apt-get update", shell=True)
+        subprocess.run("sudo apt-get upgrade", shell=True)
 
     # 更新HomeAssistant
     @staticmethod
@@ -98,7 +97,7 @@ class Service:
     # 安装HomeAssistant
     @staticmethod
     def install_ha():
-        subprocess.run("sudo pip3 install homeassistant")
+        subprocess.run("sudo pip3 install homeassistant", shell=True)
 
     # HA 自启动
     def ha_auto_start(self):
@@ -112,15 +111,36 @@ class Service:
                     "ExecStart=/usr/local/bin/hass\n\n"
                     "[Install]\n"
                     "WantedBy=multi-user.target")
+        Logger.info("Write auto start file success")
 
     # samba安装
     def samba(self):
         subprocess.run("sudo apt-get install samba samba-common", shell=True)
-        subprocess.run("sudo smbpasswd -a pi")
-
-        with fileinput.FileInput(self.smb_conf_add, inplace=True, backup='.bak') as file:
-            for line in file:
-                print(line.replace("read_only = false", "read_only = true"), end='')
+        subprocess.run("sudo smbpasswd -a pi", shell=True)
+        subprocess.run("sudo rm " + self.smb_conf_add, shell=True)
+        with open(self.smb_conf_add, "w") as f:
+            f.write("[global]\n"
+                    "netbios name = raspberrypi\n"
+                    "server string = The Pi File Center\n"
+                    "workgroup = WORKGROUP\n"
+                    "hosts allow =\n"
+                    "remote announce =\n"
+                    "remote browse sync =\n"
+                    "[HOME ASSISTANT]\n"
+                    "path = /home/pi\n"
+                    "comment = No comment\n"
+                    "browsable = yes\n"
+                    "read only = no\n"
+                    "valid users =\n"
+                    "writable = yes\n"
+                    "guest ok = yes\n"
+                    "public = yes\n"
+                    "create mask = 0777\n"
+                    "directory mask = 0777\n"
+                    "force user = root\n"
+                    "force create mode = 0777\n"
+                    "force directory mode = 0777\n"
+                    "hosts allow =\n")
         subprocess.run("sudo systemctl restart smbd", shell=True)
 
     # 字体和输入法安装
@@ -133,22 +153,23 @@ class Service:
     @staticmethod
     def mosquitto():
         subprocess.run("sudo apt-get install mosquitto", shell=True)
+        subprocess.run("sudo systemctl start mosquitto.service", shell=True)
 
 
 class Install:
     import argparse
     import sys
     parser = argparse.ArgumentParser(description="HomeAssistant Install Script")
-    parser.add_argument("-w", "--wifi", help="配置wifi信息")
-    parser.add_argument("-v", "--version", help="获取版本信心")
-    parser.add_argument("-s", "--source", help="更换apt,pip源")
-    parser.add_argument("-p", "--prepare", help="更新软件列表与软件")
-    parser.add_argument("-i", "--installHA", help="安装HomeAssistant")
-    parser.add_argument("-u", "--updateHA", help="更新HA版本")
-    parser.add_argument("-a", "--autostart", help="HA自启动")
-    parser.add_argument("-sa", "--samba", help="samba安装与配置")
-    parser.add_argument("-c", "--chinese", help="中文及输入法安装")
-    parser.add_argument("-m", "--mosquitto", help="MQTT Broker安装")
+    parser.add_argument("-w", "--wifi", default=False, help="配置wifi信息")
+    parser.add_argument("-s", "--source", default=False, help="更换apt,pip源")
+    parser.add_argument("-p", "--prepare", default=False, help="更新软件列表与软件")
+    parser.add_argument("-i", "--installHA", default=False, help="安装HomeAssistant")
+    parser.add_argument("-v", "--version", default=False, help="获取版本信息")
+    parser.add_argument("-u", "--updateHA", default=False, help="更新HA版本及其依赖")
+    parser.add_argument("-a", "--autostart", default=False, help="HA自启动")
+    parser.add_argument("-c", "--chinese", default=False, help="中文及输入法安装")
+    parser.add_argument("-m", "--mosquitto", default=False, help="MQTT Broker安装")
+    parser.add_argument("-sa", "--samba", default=False, help="samba安装与配置")
 
     args = parser.parse_args()
     args = vars(args)
