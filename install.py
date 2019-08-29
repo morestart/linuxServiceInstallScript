e4233e9ae5dae2970200e7eeee745a02b0f3900f
CTL <35556811+morestart@users.noreply.github.com>
2019-08-29 11:16:24 +0800

    Update install.py

--- install.py
+++ install.py
@@ -20,21 +20,21 @@ class Logger:
         try:
             print(Logger.OK + info + Logger.END)
         except UnicodeEncodeError:
-            Logger.warn("[ERROR] Not found chinese font, you must install chinese font, if not, we will use english..")
+            Logger.warn("[ERROR] Not found chinese font, you must install chinese font")
 
     @staticmethod
     def warn(info):
         try:
             print(Logger.WARNING + info + Logger.END)
         except UnicodeEncodeError:
-            Logger.warn("[ERROR] Not found chinese font, you must install chinese font, if not, we will use english..")
+            Logger.warn("[ERROR] Not found chinese font, you must install chinese font, if not")
 
     @staticmethod
     def error(info):
         try:
             print(Logger.FAIL + info + Logger.END)
         except UnicodeEncodeError:
-            Logger.warn("[ERROR] Not found chinese font, you must install chinese font, if not, we will use english..")
+            Logger.warn("[ERROR] Not found chinese font, you must install chinese font, if not")
 
 
 # 公共服务 各系统兼容的部分
